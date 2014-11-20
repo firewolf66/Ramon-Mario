00dea86b664e21fb4168eea8df9713d7822b7e91
Ramon De La Torre <delatorre.ramon66@gmail.com>
2014-11-20 15:04:05 -0800

    Mario Save #3

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -15,7 +15,7 @@ game.PlayerEntity = me.Entity.extend({
                   }]);
               
               this.renderable.addAnimation("idle", [3]);
-              this.renderable.addAnimation("smallWalk", [9, 10, 11, 12, 13, 14], 80);
+              this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
               this.renderable.setCurrentAnimation("idle");
             
             this.body.setVelocity(5, 20);
@@ -24,13 +24,23 @@ game.PlayerEntity = me.Entity.extend({
         update: function(delta){
             if(me.input.isKeyPressed("right")){
                 this.body.vel.x += this.body.accel.x * me.timer.tick;
+                this.renderable.setCurrentAnimation("smallWalk");
                 
             }else{
                 this.body.vel.x = 0;
                 
             }
             
+            if(this.body.vel.x !== 0) {
+                if (!this.renderable.isCurrentAnimation("smallWalk")) {
+                this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame();            }
+                }else{
+                    this.renderable.setCurrentAnimation("idle");
+                    }
+            
             this.body.update(delta);
+            this._super(me.Entity, "update", [delta]);
             return true;
 }
 
